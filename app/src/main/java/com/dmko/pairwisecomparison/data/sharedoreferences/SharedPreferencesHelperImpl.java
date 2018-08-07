bf0dd1ce593dd41199ca2de554f4b013e593e657
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2018-08-07 15:12:40 +0400

    fixed default settings values

--- app/src/main/java/com/dmko/pairwisecomparison/data/sharedoreferences/SharedPreferencesHelperImpl.java
+++ app/src/main/java/com/dmko/pairwisecomparison/data/sharedoreferences/SharedPreferencesHelperImpl.java
@@ -13,9 +13,11 @@ public class SharedPreferencesHelperImpl implements SharedPreferencesHelper {
     private String chart_colors;
 
     private SharedPreferences prefs;
+    private Resources resources;
 
     public SharedPreferencesHelperImpl(Resources resources, SharedPreferences prefs) {
         this.prefs = prefs;
+        this.resources = resources;
         prompt_text = resources.getString(R.string.key_prompt_text);
         show_percentage = resources.getString(R.string.key_show_percentage);
         default_chart = resources.getString(R.string.key_default_chart);
@@ -24,7 +26,7 @@ public class SharedPreferencesHelperImpl implements SharedPreferencesHelper {
 
     @Override
     public String getPromptText() {
-        return prefs.getString(prompt_text, null);
+        return prefs.getString(prompt_text, resources.getString(R.string.title_prompt_default));
     }
 
     @Override
@@ -34,13 +36,13 @@ public class SharedPreferencesHelperImpl implements SharedPreferencesHelper {
 
     @Override
     public int getDefaultChart() {
-        String defaultChart = prefs.getString(default_chart, null);
+        String defaultChart = prefs.getString(default_chart, "0");
         return Integer.parseInt(defaultChart);
     }
 
     @Override
     public int getChartColors() {
-        String chartColors = prefs.getString(chart_colors, null);
+        String chartColors = prefs.getString(chart_colors, "0");
         return Integer.parseInt(chartColors);
     }
 }
